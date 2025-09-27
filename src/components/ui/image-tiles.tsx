aa4e6704d68a822d47f0287d4d762803d9e1e553
x0nata <nattyalem89@gmail.com>
2025-09-28 02:32:35 +0300

    vvvvvvvvvv

--- src/components/ui/image-tiles.tsx
+++ src/components/ui/image-tiles.tsx
@@ -1,5 +1,5 @@
 import { motion, Variants } from 'framer-motion';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 interface ImageRevealProps {
     leftImage: string;
