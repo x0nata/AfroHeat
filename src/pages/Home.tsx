408cb4ea337f4de5954a204e2f1d898c6d392c9c
x0nata <nattyalem89@gmail.com>
2025-09-30 14:57:53 +0300

    strip down fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,11 @@
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
-import { useNavigate } from 'react-router-dom';
 import HeroSection from '../components/sections/HeroSection';
 import AboutSection from '../components/sections/AboutSection';
 import NewHomeSection from '../components/sections/new-home-section';
-import { Boxes } from '../components/ui/background-boxes';
 import ScheduleModal from '../components/ui/ScheduleModal';
 import BootcampModal from '../components/ui/BootcampModal';
 
 const Home: React.FC = () => {
-  const navigate = useNavigate();
   const [isScheduleOpen, setIsScheduleOpen] = useState(false);
   const [isBootcampModalOpen, setIsBootcampModalOpen] = useState(false);
   return (
