e101b113537c4b93f0859200ffa96341671e265f
x0nata <nattyalem89@gmail.com>
2025-10-08 16:36:28 +0300

    ahhhhhhhhh

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -20,7 +20,7 @@ const Footer: React.FC = () => {
   ];
 
   return (
-    <footer className="relative w-full bg-background dark:bg-[hsla(300, 94%, 6%, 1.00)] text-black-800 py-6 px-6 sm:py-10 sm:px-20 lg:px-20">
+    <footer className="relative w-full bg-background dark:bg-[hsla(300, 94%, 6%, 1.00)] text-black-800 py-3 px-6 sm:py-5 sm:px-20 lg:px-20">
       {/* Mobile Background Pattern */}
       <div
         style={{
@@ -44,55 +44,37 @@ const Footer: React.FC = () => {
 
       <div className="relative z-10">
         {/* Main Footer Content */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 sm:gap-8 mb-8 sm:mb-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 sm:gap-8 mb-4 sm:mb-4 text-left">
           {/* Brand Section */}
-          <div className="lg:col-span-2">
-            <div className="flex items-center  mb-6">
+          <div>
+            <div className="flex items-center justify-start mb-3">
               <img
                 src="/images/logos/navtitle.webp"
                 alt="AfroHeat Fitness Title"
                 className="h-14 sm:h-23 w-auto max-w-full max-h-full"
               />
             </div>
-            <p className="font-poppins font-bold mb-6 text-sm sm:text-base text-black-600 max-w-md">
+            <p className="font-poppins font-bold mb-2 text-sm sm:text-base text-black-600 max-w-md ml-0 md:ml-0 md:max-w-none">
               Powered by women, built on community, fueled by connection.
             </p>
-            <div className="space-y-2 sm:space-y-3 font-poppins">
-              <div className="flex items-center space-x-2 sm:space-x-3 text-sm sm:text-base text-black-1000 ">
+            <div className="space-y-1 sm:space-y-2 font-poppins ml-0 md:ml-0 max-w-xs md:max-w-none">
+              <div className="flex items-center justify-start space-x-2 sm:space-x-3 text-sm sm:text-base text-black-1000 ">
                 <IconMapPin className="h-3 w-3 sm:h-5 sm:w-5 " />
                 <span>Wello sefer, Ethio China St, Addis Ababa</span>
               </div>
-              <div className="flex items-center space-x-2 sm:space-x-3 text-sm sm:text-base text-black-1000 ">
+              <div className="flex items-center justify-start space-x-2 sm:space-x-3 text-sm sm:text-base text-black-1000 ">
                 <IconPhone className="h-3 w-3 sm:h-5 sm:w-5 " />
                 <span>+251 90 424 2222</span>
               </div>
             </div>
           </div>
 
-          {/* Quick Links */}
-          <div>
-            <h3 className="font-industry text-lg sm:text-xl uppercase mb-6 mt-5 text-black-1000 font-bold">Quick Links</h3>
-            <ul className="space-y-2 sm:space-y-3 font-poppins">
-              {navItems.map((item, index) => (
-                <li key={index}>
-                  <a
-                    href={item.link}
-                    className="flex items-center space-x-1.5 sm:space-x-2 text-sm sm:text-base text-black-700 hover:text-[#fab313] transition-colors "
-                  >
-                    {item.icon}
-                    <span>{item.name}</span>
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-
           {/* Social Media */}
-          <div>
-            <p className="font-poppins text-sm sm:text-base text-black-600 mb-2 mt-5 font-bold">
+          <div className="flex flex-col md:items-end justify-center">
+            <p className="font-poppins text-sm sm:text-base text-black-600 mb-5 mt-5 font-bold text-left md:text-right">
               Stay connected with our fitness community
             </p>
-            <div className="flex space-x-3 sm:space-x-4 mt-5 ml-13">
+            <div className="flex space-x-3 sm:space-x-4 mt-2 items-center  md:justify-end">
               <a href="https://www.instagram.com/afroheatfitness?igsh=NGpsc3F0YnpydjJu" target="_blank" rel="noopener noreferrer" className="hover:opacity-80 transition-opacity" aria-label="Instagram">
                 <IconBrandInstagram className="h-7 w-7 sm:h-12 sm:w-12 text-[#E1306C]" />
               </a>
