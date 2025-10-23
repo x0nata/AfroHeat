97501d752fc4a1fd506908dff546f5e4e41e35bd
x0nata <nattyalem89@gmail.com>
2025-10-23 10:38:11 +0300

    kkkkk

--- src/components/sections/MembershipSection.tsx
+++ src/components/sections/MembershipSection.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { CardSpotlight } from '../ui/card-spotlight';
 import { motion } from 'framer-motion';
 import { useApp } from '@/context/AppContext';
