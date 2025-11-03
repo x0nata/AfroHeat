c44cef7558eedde31d98404b3086767cef484503
x0nata <nattyalem89@gmail.com>
2025-11-03 22:31:34 +0300

    fffff

--- src/components/ui/ScheduleModal.tsx
+++ src/components/ui/ScheduleModal.tsx
@@ -15,19 +15,19 @@ const ScheduleModal: React.FC<ScheduleModalProps> = ({ isOpen, onClose }) => {
       kickboxing: ['6:00 PM']
     },
     tuesday: {
-      strength: ['6:00 AM', '7:00 AM', '8:00 AM', '5:00 PM', '6:00 PM', '7:00 PM'],
+      strength: ['7:00 AM', '8:00 AM', '5:00 PM', '6:00 PM', '7:00 PM'],
       kickboxing: ['7:00 AM', '8:00 AM']
     },
     wednesday: {
-      strength: ['7:00 AM', '8:00 AM', '9:00 AM','5:00 PM', '6:00 PM', '7:00 PM'],
+      strength: ['7:00 AM', '8:00 AM', '9:00 AM', '5:00 PM', '6:00 PM', '7:00 PM'],
       kickboxing: ['6:00 PM']
     },
     thursday: {
-      strength: ['6:00 AM', '7:00 AM', '8:00 AM', '5:00 PM', '6:00 PM', '7:00 PM'],
-      kickboxing: ['7:00 AM','8:00 AM']
+      strength: ['7:00 AM', '8:00 AM', '5:00 PM', '6:00 PM', '7:00 PM'],
+      kickboxing: ['7:00 AM', '8:00 AM']
     },
     friday: {
-      strength: ['7:00 AM', '8:00 AM', '9:00 AM', '5:00 PM', '6:00 PM'],
+      strength: ['7:00 AM', '8:00 AM', '9:00 AM', '5:00 PM', '6:00 PM', '7:00 PM'],
       kickboxing: ['6:00 PM']
     },
     saturday: {
